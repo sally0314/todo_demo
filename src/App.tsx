84d8c904d845b6d8a6994f60191e82f6b9c4c36d
SangminLee <sangmin@tendopay.ph>
2023-10-10 02:57:00 -0400

    week3_sangmin, Add calendar context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import Container from 'components/Container';
 // import TodoContents from 'components/TodoContents';
 import TodoModal from 'components/TodoModal';
-import { TodoContextProvider } from 'components/TodoContext';
+import {TodoContextProvider} from 'components/TodoContext';
 import TodoCalendar from "./components/TodoCalendar";
+import {TodoCalendarContextProvider} from "./contexts/TodoCalendarContext";
 
 function App() {
     return (
         <Container>
             <TodoContextProvider>
                 {/*<TodoContents />*/}
-                <TodoCalendar/>
-                <TodoModal />
+                <TodoCalendarContextProvider>
+                    <TodoCalendar/>
+                </TodoCalendarContextProvider>
+                <TodoModal/>
             </TodoContextProvider>
         </Container>
     );
--- src/components/TodoCalendar.tsx
+++ src/components/TodoCalendar.tsx
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
 import dayjs, {Dayjs} from 'dayjs';
-
+import {TodoCalendarContext} from "../contexts/TodoCalendarContext";
+import {useContext} from "react";
+import Button from "./Button";
 
 const Container = styled.div`
   width: 100%;
@@ -20,6 +22,7 @@ const MonthSelector = styled.nav`
 
 const DayHeader = styled.th`
   width: 10rem;
+  height: 3rem;
 `
 const Day = styled.td`
   width: 10rem;
@@ -28,7 +31,8 @@ const Day = styled.td`
 `
 
 const TodoCalendar = () => {
-    const workDate: Dayjs = dayjs()
+    const {settings, goPrev, goToday, goNext} = useContext(TodoCalendarContext)
+    const workDate: Dayjs = settings.currentDate || dayjs()
     const title: string = workDate.clone().format('MMMM YYYY')
     const daysInMonth: number = workDate.daysInMonth()
     // 0 (Sunday) to 6 (Saturday)
@@ -43,7 +47,7 @@ const TodoCalendar = () => {
     for (let i: number = 0; i < chunks.length; i++) {
         chunks[i] = days.splice(0, 7)
     }
-    console.log(chunks)
+
     return (
         <Container className="flex items-center justify-center">
             <table className="table-auto border-collapse border border-slate-400">
@@ -58,23 +62,35 @@ const TodoCalendar = () => {
                     <th></th>
                     <th></th>
                     <th></th>
-                    <th></th>
                     <th>
                         <MonthSelector>
                             <ul className="inline-flex -space-x-px text-sm">
                                 <li>
-                                    <a href="#"
-                                       className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                    >
-                                        Previous
-                                    </a>
+                                    <Button
+                                        label="Today"
+                                        className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                                        onClick={goToday}
+                                    />
+                                </li>
+                            </ul>
+                        </MonthSelector>
+                    </th>
+                    <th>
+                        <MonthSelector>
+                            <ul className="inline-flex -space-x-px text-sm">
+                                <li>
+                                    <Button
+                                        label="Prev"
+                                        className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                                        onClick={goPrev}
+                                    />
                                 </li>
                                 <li>
-                                    <a href="#"
-                                       className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                    >
-                                        Next
-                                    </a>
+                                    <Button
+                                        label="Next"
+                                        className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                                        onClick={goNext}
+                                    />
                                 </li>
                             </ul>
                         </MonthSelector>
@@ -97,7 +113,7 @@ const TodoCalendar = () => {
                             {row.map((x: number, j: number) => {
                                 return (
                                     <Day className="border border-slate-300" key={`col-${j}`}>
-                                        {x === 0 ? '' : x}
+                                        <div>{x === 0 ? '' : x}</div>
                                     </Day>)
                             })}
                         </tr>
