82b792bf2016fdc0c6d4160a1d9f699532aac0af
SangminLee <sangmin@tendopay.ph>
2023-10-23 19:49:18 -0400

    week5, Install tw-element

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}',
+        './node_modules/tw-elements/dist/js/**/*.js'
+    ],
+    plugins: [
+        require('tw-elements/dist/plugin.cjs'),
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
 }
